7706106314e0fbc9fe3fbc96da7f88a98fc57b8e
wangye <1801265@qq.com>
2026-09-15 13:33:07 +0800

    Match site logo to favicon

--- pet-metric-ui/app/components/BrandMark.vue
+++ pet-metric-ui/app/components/BrandMark.vue
@@ -1,5 +1,5 @@
 <template>
   <span class="brand-mark" aria-hidden="true">
-    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round"><path d="M9 3H5a2 2 0 0 0-2 2v4m6-6h10a2 2 0 0 1 2 2v4M9 3v18m0 0h10a2 2 0 0 0 2-2V9M9 21H5a2 2 0 0 1-2-2V9m0 0h18" /></svg>
+    <img src="/favicon.svg" alt="" width="64" height="64" />
   </span>
 </template>
